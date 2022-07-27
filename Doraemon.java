83a19936927586b73f7469241db28bebc8df5b6e
qashmal <110073468+qashmal@users.noreply.github.com>
2022-07-27 08:29:41 +0700

    Create Doraemon.java

---
+++ Doraemon.java
@@ -0,0 +1,20 @@
+package doraemonn;
+
+import java.util.ArrayList;
+
+public class Doraemonn {
+
+    public static void main(String[] args) {
+        ArrayList kantongAjaib = new ArrayList();
+
+        kantongAjaib.add("Pintu ke masa depan");
+        kantongAjaib.add("bersama");
+        kantongAjaib.add("qasmal");
+        kantongAjaib.add("hehe");
+        kantongAjaib.add(true);
+
+        System.out.println(kantongAjaib);
+        System.out.println(kantongAjaib.size());
+
+    }
+}
